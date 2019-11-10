d773b1e23a1abf7afd47b11cb8cceaf7835275f8
kangwubin <2624656980@qq.com>
2019-11-10 15:39:52 +0800

    增加代码

---
+++ Homework_day9/src/cn/xatu/Test.java
@@ -0,0 +1,13 @@
+package cn.xatu;
+
+/**
+ * Description:
+ *
+ * @author: KangWuBin
+ * @Date: 2019/11/10 15:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
